f92de337a5c32ec7792aa71bf911dfa419cfb16a
nkBrew <nicholasjwb@gmail.com>
2021-11-23 13:17:57 -0800

    Initial commit with typescript, eslint, prettier, and data, and assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
   ],
-}
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+
+const IndexPage = () => <div>I132312</div>;
+
+export default IndexPage;
